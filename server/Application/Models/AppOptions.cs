f114f55bfaa4d1213a2154bc8cea88b29416d495
Taja992 <Tharaxui@gmail.com>
2025-05-23 15:48:59 +0200

    Cleaned up unused things in AppOptions

--- server/Application/Models/AppOptions.cs
+++ server/Application/Models/AppOptions.cs
@@ -15,17 +15,8 @@ namespace Application.Models
     }
     
     public class AppOptions
-    {
-        public int PORT { get; set; } = 8080;
-        public int REST_PORT { get; set; } = 5000;
-        public int WS_PORT { get; set; } = 5001;
-        
-        // public JwtSettings JwtSettings { get; set; } = new JwtSettings();
-        
+    {        
         public string POSTGRES_CONNECTION_STRING { get; set; } = "Host=localhost;Database=pictionary;Username=postgres;Password=postgres";
-        
-        public bool Seed { get; set; } = false;
-        
         public WebSocketOptions WebSocket { get; set; } = new WebSocketOptions();
     }
 }
